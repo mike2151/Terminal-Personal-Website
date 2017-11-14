8dcf27a5e23f07272e9d10c51aa1ac6c9bd7cb2f
mike2151 <redefiningproductions@gmail.com>
2017-11-14 11:38:42 -0500

    Updated UI and made generating HTML function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+//initial populate
+generateHTML("all");
+
 //terminal code: http://terminal.jcubic.pl/
 jQuery(function($, undefined) {
    $('#terminal').terminal(function(command) {
@@ -12,3 +15,28 @@ jQuery(function($, undefined) {
         prompt: '> '});
         
 });
+
+function generateHTML(keyword) {
+    $.getJSON("public/content.json", function(json) {
+        console.log(json); 
+    });
+    
+    $("#content").empty();
+    $.tmpl( '<div class="entry_odd"> \
+          <div class="col-md-6">\
+              <img src="">\
+          </div>\
+          <div class="col-md-6">\
+              <div class="description">\
+                <h1 class="header-description">Header</h1>\
+                <h3 class="sub-description">Tiny Project</h3>\
+                <div class="divider"></div>  \
+                <p class="item-description">\
+                  <ul>\
+                    <li>Bullet</li>\
+                  </ul>  \
+                </p>  \
+              </div>\
+          </div>\
+      </div>', { "Name" : "John Doe" }).appendTo( "#content" );
+}
