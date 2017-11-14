6090de686619a944e88d82517ceb2a1830476a82
mike2151 <redefiningproductions@gmail.com>
2017-11-14 12:27:58 -0500

    json parsing

--- js/main.js
+++ js/main.js
@@ -17,26 +17,25 @@ jQuery(function($, undefined) {
 });
 
 function generateHTML(keyword) {
-    $.getJSON("public/content.json", function(json) {
-        console.log(json); 
-    });
-    
-    $("#content").empty();
-    $.tmpl( '<div class="entry_odd"> \
+    $.getJSON("https://raw.githubusercontent.com/mike2151/Terminal-Personal-Website/master/public/content.json", function(json) {
+        console.log(json[keyword].entry);
+         $("#content").empty();
+         $.tmpl( '<div class="entry_odd"> \
           <div class="col-md-6">\
-              <img src="">\
+              <img src="' + json[keyword].entry.img + '">\
           </div>\
           <div class="col-md-6">\
               <div class="description">\
-                <h1 class="header-description">Header</h1>\
-                <h3 class="sub-description">Tiny Project</h3>\
+                <h1 class="header-description">' + json[keyword].entry.title + '</h1>\
+                <h3 class="sub-description">'+ json[keyword].entry.miniDescription + '</h3>\
                 <div class="divider"></div>  \
                 <p class="item-description">\
                   <ul>\
-                    <li>Bullet</li>\
+                    <li>' + json[keyword].entry.description + '</li>\
                   </ul>  \
                 </p>  \
               </div>\
           </div>\
       </div>', { "Name" : "John Doe" }).appendTo( "#content" );
+    });
 }
