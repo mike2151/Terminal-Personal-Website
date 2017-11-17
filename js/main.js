6a21ce648cd4ea25a5b657e787ac01141c280e03
mike2151 <redefiningproductions@gmail.com>
2017-11-16 23:00:51 -0500

    made alternating homepage

--- js/main.js
+++ js/main.js
@@ -25,27 +25,51 @@ $(function() {
 });
 
 
-
 function generateHTML(keyword) {
     $.getJSON("https://raw.githubusercontent.com/mike2151/Terminal-Personal-Website/master/public/content.json", function(json) {
-         console.log(json);
-         $("#content").empty();
-         $.tmpl( '<div class="entry_odd"> \
-          <div class="col-md-6">\
-              <img class="img-desc" src="' + json[keyword].entry.img + '">\
-          </div>\
-          <div class="col-md-6">\
-              <div class="description">\
-                <h1 class="header-description">' + json[keyword].entry.title + '</h1>\
-                <h3 class="sub-description">'+ json[keyword].entry.miniDescription + '</h3>\
-                <div class="divider"></div>  \
-                <p class="item-description">\
-                  <ul>\
-                    <li>' + json[keyword].entry.description + '</li>\
-                  </ul>  \
-                </p>  \
+        $("#content").empty();
+         var state_counter = 0;
+         for(var entry in json[keyword]) {
+             if (state_counter % 2 == 0) {
+                 $.tmpl( '<div class="entry_odd"> \
+              <div class="col-md-6">\
+                  <img class="img-desc" src="' + json[keyword][entry].img + '">\
               </div>\
-          </div>\
-      </div>', { "Name" : "John Doe" }).appendTo( "#content" );
+              <div class="col-md-6">\
+                  <div class="description">\
+                    <h1 class="header-description">' + json[keyword][entry].title + '</h1>\
+                    <h3 class="sub-description">'+ json[keyword][entry].miniDescription + '</h3>\
+                    <div class="divider"></div>  \
+                    <p class="item-description">\
+                      <ul>\
+                        <li>' + json[keyword][entry].description + '</li>\
+                      </ul>  \
+                    </p>  \
+                  </div>\
+              </div>\
+          </div>', { "Name" : "John Doe" }).appendTo( "#content" );
+             }
+             else {
+                 $.tmpl( '<div class="entry_even"> \
+              <div class="col-md-6">\
+                  <div class="description">\
+                    <h1 class="header-description">' + json[keyword][entry].title + '</h1>\
+                    <h3 class="sub-description">'+ json[keyword][entry].miniDescription + '</h3>\
+                    <div class="divider"></div>  \
+                    <p class="item-description">\
+                      <ul>\
+                        <li>' + json[keyword][entry].description + '</li>\
+                      </ul>  \
+                    </p>  \
+                  </div>\
+              </div>\
+              <div class="col-md-6">\
+                  <img class="img-desc" src="' + json[keyword][entry].img + '">\
+              </div>\
+          </div>', { "Name" : "John Doe" }).appendTo( "#content" );
+             }
+             state_counter = state_counter + 1;
+             
+        }
     });
 }
